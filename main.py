b67947d5cf145da2f0bdd5b561b2fa2727562617
Sand <sandboxasdf@gmail.com>
2025-10-01 13:20:54 +1000

    error checking and command line arguments

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
+from pathlib import Path
+import sys
+
 # Write a new function called get_book_text. It takes a filepath as input and returns the contents of the file as a string.
 def OpenStringFile(filepath):
+    my_file = Path(filepath)
+    if not my_file.is_file():
+        # print("File does not exist")
+        return "ERROR: File does not exist"
+    
+    
     with open(filepath) as f:
-        # do something with f (the file) here
         contents = f.read()
         return contents
     
 def main():
-    print(OpenStringFile("books/frankenstein.txt"))
+    if (len(sys.argv) < 2):
+        print("USAGE:\npython3 main.py [book.txt]")
+        return
+    print(OpenStringFile(sys.argv[1]))
     
 main()
