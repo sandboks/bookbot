712f883cff2e96160ec5d38f5cc283ed1441b0f5
Sand <sandboxasdf@gmail.com>
2025-10-01 13:04:17 +1000

    read txt file as string and print it out

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings world")
+# Write a new function called get_book_text. It takes a filepath as input and returns the contents of the file as a string.
+def OpenStringFile(filepath):
+    with open(filepath) as f:
+        # do something with f (the file) here
+        contents = f.read()
+        return contents
+    
+def main():
+    print(OpenStringFile("books/frankenstein.txt"))
+    
+main()
